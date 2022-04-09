e3fa36354f852a7d287154db4a21d28c3bee4e16
sirasit <sirasit@siruxsolutions.com>
2022-04-09 15:22:12 -0700

    Added Styles.css, added todo functionality

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+var items = ["Wake up", "Code", "Eat", "Exercise", "Chore", "Repeat"];
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    var today = new Date();
+
+    var options = {
+      weekday: "long",
+      day: "numeric",
+      month:"long",
+    };
+    var day = today.toLocaleDateString("en-US", options);
+    res.render("list", {kindOfDay: day, newListItem: items});
+});
+
+app.post("/", function(req, res){
+    items.push(req.body.newItem);
+    res.redirect("/");
+});
+app.listen(3000, function(){
+    console.log("Server running on port 3000");
+});
